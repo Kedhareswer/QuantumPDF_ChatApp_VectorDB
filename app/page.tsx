901371f398935843fb5858ebf30c5a8a4202f905
Kedhareswer <Kedhareswer.1211062>
2025-10-11 19:27:08 +0530

    mobile-responsivness-1

--- app/page.tsx
+++ app/page.tsx
@@ -659,8 +659,8 @@ export default function QuantumPDFChatbot() {
         <div
           className={`
           fixed lg:relative inset-y-0 left-0 z-40
-          ${sidebarCollapsed ? "lg:w-16 w-72" : "w-80"}
-          ${sidebarOpen ? "translate-x-0" : "-translate-x-full lg:translate-x-0"}
+          ${sidebarCollapsed ? "lg:w-16" : "lg:w-80"}
+          ${sidebarOpen ? "w-full lg:w-80 translate-x-0" : "w-full lg:w-80 -translate-x-full lg:translate-x-0"}
           transition-all duration-300 ease-in-out
           bg-white border-r-2 border-black flex flex-col h-screen
         `}
